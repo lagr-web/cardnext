996b89b0a3e811cc64b692b5281c2c1f3c4b5fd2
Lars Gregersen <lagr@videndjurs.dk>
2025-01-08 10:54:59 +0100

    finished

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import footerstyle from "@/components/myfooter.module.scss"
+
+const Footer = () => {
+
+    return (
+        <footer id={footerstyle.footer}>Cool Ninja Kickass Footer</footer>
+    )
+}
+
+export default Footer;
